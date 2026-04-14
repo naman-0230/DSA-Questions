7f6e2a64383dfe46b2e101579e79b48af80ee928
naman-0230 <namansharmaa2305@gmail.com>
2026-04-15 01:32:26 +0530

    kinda getting grasp of maps

---
+++ arrays.cpp/hash_mapsANDsets/twoSum.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+int main(){
+    vector<int> v={2,4,3,1,6,7,8,9};
+ int target = 4;
+unordered_map<int,int> m;
+
+for(int i=0; i<v.size(); i++){                        //still clearing basics 
+    int remaining = target - v[i];
+
+    if(m.find(remaining) != m.end()){
+     cout<< m[remaining]<<" "<<i;
+    }
+    m[v[i]]=i;
+
+}
+return 0;
+}
