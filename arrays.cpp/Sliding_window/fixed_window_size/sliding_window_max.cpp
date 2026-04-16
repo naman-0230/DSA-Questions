866616d1a87c2401dd7e0339cbda9c140610bf59
naman-0230 <namansharmaa2305@gmail.com>
2026-04-17 00:00:17 +0530

    imp question

---
+++ arrays.cpp/Sliding_window/fixed_window_size/sliding_window_max.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<deque>
+#include<climits>
+using namespace std;
+
+int main(){
+ vector<int> v ={2,3,5,1,-2,4,5,3,-7,8,2,3,1};
+int k = 3;
+
+vector<int> ans;
+deque<int> dq;
+int max = INT_MIN;
+
+
+for(int i=0; i<k; i++){                     //first k elements
+
+  while(!dq.empty() && dq.back() <= v[i]){
+    dq.pop_back();
+    }
+dq.push_back(i);
+  }
+
+  ans.push_back(v[dq.front()]);
+
+for(int i=k; i< v.size() ;i++){             //sliding window
+ while(!dq.empty() && dq.front() <= i-k){    //removing unnecessary elements
+    dq.pop_front();
+ }
+ 
+ while(!dq.empty() && v[dq.back()] <= v[i]){      //new elements
+    dq.pop_back();}      
+    
+  dq.push_back(i);  
+  
+  ans.push_back(v[dq.front()]);
+ 
+}
+for(int x : ans){
+    cout<<x <<" ";
+}
+
+
+return 0;
+}
