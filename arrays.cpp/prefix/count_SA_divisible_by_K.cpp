f74fb57145ab7c4f6fa8b9d6e47e5745c5b508a2
naman-0230 <namansharmaa2305@gmail.com>
2026-04-19 03:21:41 +0530

    another one by practice

---
+++ arrays.cpp/prefix/count_SA_divisible_by_K.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+int countSA(vector<int> &v , int k){       
+unordered_map<int, int> m;                
+
+ int sum = 0;
+int count = 0;
+
+   m[0] = 1;                       // imp comdition for prefix[-1] = 0 
+ for(int i=0; i< v.size(); i++){
+    sum += v[i];
+    int rem = sum % k;
+
+    if(rem< 0){
+      rem += k;
+    }
+    if(m.find(rem - 0)!=m.end()){
+      count += m[rem - 0];
+    }    
+    m[rem]++;                           
+ }
+ return count;
+
+}
+int main (){
+  vector<int> v ={1,2,0,1,1,0,3,0,1};
+ int k=2;
+ 
+ cout<<countSA(v,k);
+    return 0;
+}
