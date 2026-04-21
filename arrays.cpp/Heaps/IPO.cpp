d2d6e25194676066289e3ddbe4c30c963f5a1fe9
naman-0230 <namansharmaa2305@gmail.com>
2026-04-22 01:04:32 +0530

    hard but easy

---
+++ arrays.cpp/Heaps/IPO.cpp
@@ -0,0 +1,47 @@
+
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+int main() {
+    int k = 2, w = 2;
+    vector<int> profits = {1,2,3,5,6};
+    vector<int> capital = {0,1,1,6,8};
+
+    // min heap based on capital
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> minH;
+
+    // max heap for profits
+    priority_queue<int> maxH;
+
+    // push all projects into min heap
+    for(int i = 0; i < profits.size(); i++) {
+        minH.push({capital[i], profits[i]});
+    }
+
+    while(k--) {
+
+        // unlock all projects we can afford
+        while(!minH.empty() && minH.top().first <= w) {
+            maxH.push(minH.top().second);
+            minH.pop();
+        }
+
+        // if no project available
+        if(maxH.empty()) break;
+
+        // pick most profitable
+        w += maxH.top();
+        maxH.pop();
+    }
+
+    cout << w;
+    return 0;
+}
+
+
+
+
+
+
