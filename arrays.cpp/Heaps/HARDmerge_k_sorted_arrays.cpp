fb186537b803cdc27c3cb030307b54422731e749
naman-0230 <namansharmaa2305@gmail.com>
2026-04-21 23:42:23 +0530

    did it with imp unpacking tuple

--- arrays.cpp/Heaps/HARDmerge_k_sorted_arrays.cpp
+++ arrays.cpp/Heaps/HARDmerge_k_sorted_arrays.cpp
@@ -3,45 +3,40 @@
 #include<unordered_map>
 #include<queue>
 #include<algorithm>
+#include<tuple>
 using namespace std;
-
+ 
 int main(){
-   vector<int> v = {6, 5, 3, 2, 8, 10, 9};
-   int k = 2;
-   
- priority_queue<int, vector<int>, greater<int>> pq;
-
-    vector<int> ans;
-
-    // first k+1 elements
-    for(int i = 0; i <= k; i++) {
-        pq.push(v[i]);
-    }
-
-    // process remaining
-    for(int i = k+1; i < v.size(); i++) {
-        ans.push_back(pq.top());
-        pq.pop();
-
-        pq.push(v[i]);
-    }
-
-    // remaining heap
-    while(!pq.empty()) {
-        ans.push_back(pq.top());
-        pq.pop();
-    }
-
-    for(int x : ans) cout << x << " ";
+ 
+    vector<vector<int>> v = {
+        {1,4,7},
+        {2,5,8},
+        {3,6,9}};
     
+    vector<int>ans;
+    priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>, greater<tuple<int,int,int>>> pq;    
+    
+for(int i =0; i< v.size(); i++){
+    pq.push({v[i][0] , i , 0});
+}
+
+while(!pq.empty()){
+  auto t = pq.top();       //// unpacking the tuple// imp concept
+
+int val = get<0>(t);
+int arr = get<1>(t);
+int idx = get<2>(t);
+
+ans.push_back(val); 
+pq.pop();
+if(idx + 1  < v[arr].size()){                         //imp to see how we check inner arr size 
+    pq.push({v[arr][idx+1], arr , idx+1});
+}
+
+}
+   for(int x : ans){
+    cout<< x;
+   }
 
     return 0;
 }
-
-
-
-
-
-
-
-
