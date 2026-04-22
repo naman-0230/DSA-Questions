cadea5f0e4c428a50ff83a7464c92bff4d8a83b1
naman-0230 <namansharmaa2305@gmail.com>
2026-04-23 02:35:38 +0530

    duplicates added

--- arrays.cpp/binarySearch/binaryPeakElement.cpp
+++ arrays.cpp/binarySearch/binaryPeakElement.cpp
@@ -2,15 +2,21 @@
 using namespace std;
 
 int findPeak(int arr[], int n) {
-    int i = 0, j = n - 1;
-
+    int i = 0, j = n - 1;            
     while (i < j) {
         int mid = i + (j - i) / 2;
 
         if (arr[mid] < arr[mid + 1]) {
             i = mid + 1;   // move right
-        } else {
+        } 
+        else if(arr[mid] > arr[mid + 1]){
             j = mid;       // move left (including mid)
+        }
+        else{
+            j--;            ///// for duplicates buttt it doesnt gurantee global peak
+                                 // only valid one e.g [1, 2, 2, 2, 3]
+                                 //if we do j-- >> lost access to [3]..
+                                 //for sure method is not binary search then
         }
     }
 
