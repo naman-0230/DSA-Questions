b87289968fb357b110b3aa5539068b12be78d73c
naman-0230 <namansharmaa2305@gmail.com>
2026-04-26 13:17:54 +0530

    basics of queues

--- Stacks_Queues/basics.cpp
+++ Stacks_Queues/basics.cpp
@@ -28,3 +28,48 @@ public:
         return top == -1;
     }
 };
+
+
+class Queue {
+    int *arr;
+    int front, rear, size, capacity;
+
+public:
+    Queue(int cap) {
+        capacity = cap;
+        arr = new int[cap];
+        front = 0;
+        rear = -1;
+        size = 0;
+    }
+
+    void push(int x) {
+        if (size == capacity) {
+            cout << "Queue Full\n";
+            return;
+        }
+
+        rear = (rear + 1) % capacity;
+        arr[rear] = x;
+        size++;
+    }
+
+    void pop() {
+        if (size == 0) {
+            cout << "Queue Empty\n";
+            return;
+        }
+
+        front = (front + 1) % capacity;
+        size--;
+    }
+
+    int Front() {
+        if (size == 0) return -1;
+        return arr[front];
+    }
+
+    bool empty() {
+        return size == 0;
+    }
+};
