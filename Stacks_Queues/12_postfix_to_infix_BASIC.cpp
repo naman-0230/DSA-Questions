f84ab0715ac0b7d35740a1f0d1e620c45ab395de
naman-0230 <namansharmaa2305@gmail.com>
2026-05-01 02:33:08 +0530

    nevermind i improved it right now

--- Stacks_Queues/12_postfix_to_infix_BASIC.cpp
+++ Stacks_Queues/12_postfix_to_infix_BASIC.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<stack>
 #include<string>
+#include<cctype>
 using namespace std;
 
 string postfixToInfix(string s){
@@ -12,10 +13,23 @@ string postfixToInfix(string s){
         if(c == ' ') continue;
 
         // operand
-        if(isalnum(c)){
-            st.push(string(1, c));
+        if(isdigit(c)){
+            int num = 0;
+            while(i< s.size() && isdigit(s[i])){   //for handling multidigit numbers
+            num = num*10 + (s[i]-'0');
+            i++;    
         }
+        i--;
+            st.push(to_string(num));
+        }
+        //for alpha
+        else if(isalpha(c)){
+            st.push(string(1,c));
+        }
+
         else{ // operator
+            if(st.size() < 2) return "invalid expression";
+
             string a = st.top(); st.pop(); // right
             string b = st.top(); st.pop(); // left
 
@@ -23,11 +37,11 @@ string postfixToInfix(string s){
             st.push(exp);
         }
     }
-
+    if(st.size()!= 1) return "invlaid expression";
     return st.top();
 }
 
 int main(){
-    string s = "234+*7*";
+    string s = "2 3 c + * 7 *";
     cout << postfixToInfix(s);
 }
