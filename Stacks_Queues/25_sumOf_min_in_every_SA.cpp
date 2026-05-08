625de98c8444a0639ab4273203ba95e0252ad40c
naman-0230 <namansharmaa2305@gmail.com>
2026-05-08 16:18:39 +0530

    different logic of contribution

---
+++ Stacks_Queues/25_sumOf_min_in_every_SA.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+#include<string>
+using namespace std;
+
+vector<int> lastSmaller(vector<int> &v){
+ vector<int> lastSmaller(v.size(), -1);
+    stack<int> st;
+
+    for(int i= v.size()-1 ; i>= 0 ;i--){
+        while(!st.empty() && v[st.top()] > v[i]){  
+            lastSmaller[st.top()] = i;
+            st.pop();
+        }
+        st.push(i);
+    }
+    return lastSmaller;
+}
+
+vector<int> nextSmaller(vector<int> &v){
+    vector<int> nextSmaller(v.size(), v.size());   
+    stack<int> st;
+
+    for(int i=0;i<v.size();i++){
+        while(!st.empty() && v[st.top()] >= v[i]){        /////imp to put >====== in next smaller
+            nextSmaller[st.top()] = i;
+            st.pop();
+        }
+        st.push(i);
+    }
+    return nextSmaller;
+}
+
+int sumOfMin(vector<int> &v){
+    int sumMin = 0;
+    vector<int> next = nextSmaller(v);
+    vector<int> last = lastSmaller(v);
+    
+    for(int i=0; i< v.size(); i++){
+        int numberSA = (i-last[i]) * (next[i]-i);
+        int contribution = v[i] * numberSA;
+        sumMin += contribution;
+    }
+    return sumMin;
+}
+
+int main(){
+    vector<int> v = {2,2,2};
+    cout<< sumOfMin(v);
+    return 0;
+}
