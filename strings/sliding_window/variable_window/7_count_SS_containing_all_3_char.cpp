56c5ea036f5f76b3f93c6b000c709806d5d1010d
naman-0230 <namansharmaa2305@gmail.com>
2026-05-19 00:18:44 +0530

    greater than equal to logic with different count

---
+++ strings/sliding_window/variable_window/7_count_SS_containing_all_3_char.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<unordered_map>
+using namespace std;
+int countSS(string &s){
+    int count = 0;
+    int j = 0;
+    int countA = 0, countB = 0, countC = 0; 
+
+    for(int i=0; i<s.size(); i++){
+
+        if(s[i] == 'a') countA++;
+        else if(s[i] == 'b') countB++;
+        else countC++;
+
+        while(countA >= 1 && countB >= 1 && countC >= 1){
+            
+            count += s.size() - i;            //// important counttttt  .. also imp to do count one time inside while not before while too
+
+            if(s[j] == 'a') countA--;
+            else if(s[j] == 'b') countB--;
+            else countC--;
+
+            j++;
+
+        }
+
+    }
+    return count;
+}
+
+int main(){
+
+    string s = {"abcabaaaccccbccaaabc"};
+    cout<< countSS(s);
+
+return 0;
+}
