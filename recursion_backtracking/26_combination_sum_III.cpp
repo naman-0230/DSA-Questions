cb42a3c77d7453a87e6543e34ffa3b039ebec952
naman-0230 <namansharmaa2305@gmail.com>
2026-06-20 13:21:39 +0530

    done this one

---
+++ recursion_backtracking/26_combination_sum_III.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+vector<vector<int>> ans;
+
+void combo(int k, int n, int start, vector<int> &curr, int &curr_sum){
+    if(curr_sum > n) return;
+
+    if(curr_sum == n && curr.size() == k){
+        ans.push_back(curr);
+        return;
+    }
+
+    if(curr.size() > k) return;
+
+    for(int i= start; i<=9; i++){  //grasp the start logic cause its the only bug before
+    //not using used and just using start one cause if we use used and only skip the previous number 
+    //we will get 1 2 3, 2 1 3, 3 1 2 kinda answer but we want unique permutation
+
+        if(curr_sum + i > n) break;
+
+        curr.push_back(i);
+        curr_sum += i;
+        combo(k, n, i+1, curr, curr_sum);
+
+        curr.pop_back();
+        curr_sum -= i;
+    }
+}
+
+int main(){
+    int k = 3;
+    int n = 7;
+    vector<int> curr;
+    int curr_sum = 0;
+
+    combo(k,n,0,curr,curr_sum);
+
+    for(auto x: ans){
+        for(int y:x){
+            cout<<y<<" ";
+        }
+        cout<<endl;
+    }
+
+return 0;
+}
